179f49a8bd6e243b5c1688b79eb1a50a52df3c80
rogelio1502 <rogelio15021515@gmail.com>
2022-01-25 22:13:55 -0600

    Added default values and some fields in User, such as the relationships added_by with articles and categories model

--- models/Models.py
+++ models/Models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from . import db
 from .base_model import Base
 from models import base_model
@@ -13,6 +14,10 @@ class Article(db.Model,Base):
         db.Integer, db.ForeignKey('categories.id'),
         nullable=False
     )
+    added_by = db.Column(
+        db.Integer, db.ForeignKey('user.id'),
+        nullable=False, default=4
+    )
     db.UniqueConstraint(name)    
     
 class Category(db.Model,Base):
@@ -21,11 +26,14 @@ class Category(db.Model,Base):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50),nullable=False)
-    status = db.Column(db.Boolean, default=True, nullable=False)
+    status = db.Column(db.Boolean, default=1, server_default="1", nullable=False)
     articles = db.relationship(
         'Article',backref="category", lazy=True
     )
-    
+    added_by = db.Column(
+        db.Integer, db.ForeignKey('user.id'),
+        nullable=True, default=4, server_default="4"
+    )
     db.UniqueConstraint(name)
 
 class User(db.Model, Base):
@@ -35,3 +43,15 @@ class User(db.Model, Base):
     last_name = db.Column(db.String(75), nullable=False)
     email = db.Column(db.String(70), unique = True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    phone_number = db.Column(db.String(10), nullable=True)
+    status = db.Column(
+        db.String(10),default="active",
+             server_default="active", nullable=False
+    )
+    
+    articles = db.relationship(
+        'Article',backref="user", lazy=True
+    )
+    categories = db.relationship(
+        'Category',backref="user", lazy=True
+    )
